a2e824d7903e604f6cfb9251b5bb1e52359d3966
pr4gn4 <30860854+pr4gn4@users.noreply.github.com>
2019-09-03 16:17:10 -0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+import VueChatScroll from "vue-chat-scroll";
+Vue.use(VueChatScroll);
+
+Vue.config.productionTip = false;
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
